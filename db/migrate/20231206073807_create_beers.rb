d9cd2387bcf1c0323cc1174b065c325002e85144
Matthew Miner <misternuks@gmail.com>
2023-12-06 17:26:27 +0900

    add beer model and table plus images

---
+++ db/migrate/20231206073807_create_beers.rb
@@ -0,0 +1,16 @@
+class CreateBeers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :beers do |t|
+      t.string :name
+      t.string :type
+      t.integer :price
+      t.text :description
+      t.boolean :new
+      t.boolean :limited
+      t.integer :count
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
